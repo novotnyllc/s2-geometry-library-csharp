1f08e5cdb951d7a006c6b9f23cdea58a8077cf76
Oren Novotny <oren@novotny.org>
2014-02-09 23:23:36 -0500

    Add missing dep to C5

--- S2Geometry/Properties/AssemblyInfo.cs
+++ S2Geometry/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("S2Geometry")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Oren Novotny 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
