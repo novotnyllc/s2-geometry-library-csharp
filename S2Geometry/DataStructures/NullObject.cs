84253dc3f7e5d54de8ca85b63c0a89a94c1a3572
Ryan Currie <dddbliss@gmail.com>
2016-07-27 09:59:38 -0700

    Removing C5 references to allow compilation on NETStandard1.0

---
+++ S2Geometry/DataStructures/NullObject.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Common.Geometry.DataStructures
+{
+    public struct NullObject<T>
+    {
+        [DefaultValue(true)]
+        private bool isnull;// default property initializers are not supported for structs
+
+        private NullObject(T item, bool isnull) : this()
+        {
+            this.isnull = isnull;
+            this.Item = item;
+        }
+
+        public NullObject(T item) : this(item, item == null)
+        {
+        }
+
+        public static NullObject<T> Null()
+        {
+            return new NullObject<T>();
+        }
+
+        public T Item { get; private set; }
+
+        public bool IsNull()
+        {
+            return this.isnull;
+        }
+
+        public static implicit operator T(NullObject<T> nullObject)
+        {
+            return nullObject.Item;
+        }
+
+        public static implicit operator NullObject<T>(T item)
+        {
+            return new NullObject<T>(item);
+        }
+
+        public override string ToString()
+        {
+            return (Item != null) ? Item.ToString() : "NULL";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return this.IsNull();
+
+            if (!(obj is NullObject<T>))
+                return false;
+
+            var no = (NullObject<T>)obj;
+
+            if (this.IsNull())
+                return no.IsNull();
+
+            if (no.IsNull())
+                return false;
+
+            return this.Item.Equals(no.Item);
+        }
+
+        public override int GetHashCode()
+        {
+            if (this.isnull)
+                return 0;
+
+            var result = Item.GetHashCode();
+
+            if (result >= 0)
+                result++;
+
+            return result;
+        }
+    }
+}
--- S2Geometry/S2Polygon.cs
+++ S2Geometry/S2Polygon.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Google.Common.Geometry.DataStructures;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using C5;
 
 namespace Google.Common.Geometry
 {
@@ -138,7 +138,7 @@ namespace Google.Common.Geometry
         {
             get
             {
-                var vertices = new HashBag<S2Point>();
+                var vertices = new HashSet<S2Point>();
 
 
                 S2Loop lastParent = null;
@@ -276,9 +276,8 @@ namespace Google.Common.Geometry
             // assert (this.loops.isEmpty());
 
             //Dictionary<S2Loop, List<S2Loop>> loopMap =new Dictionary<S2Loop, List<S2Loop>>();
-            // Note: We're using C5's HashDictionary because SCG's Dictionary<,> does not allow
-            // NULL keys
-            var loopMap = new HashDictionary<S2Loop, List<S2Loop>>();
+            // Note: We use a special NullObject to supply null keys.
+            var loopMap = new Dictionary<NullObject<S2Loop>, List<S2Loop>>();
             // Yes, a null key is valid. It is used here to refer to the root of the
             // loopMap
             loopMap[null] = new List<S2Loop>();
@@ -964,7 +963,7 @@ namespace Google.Common.Geometry
         }
 
         // For each map entry, sorts the value list.
-        private static void SortValueLoops(C5.IDictionary<S2Loop, List<S2Loop>> loopMap)
+        private static void SortValueLoops(IDictionary<NullObject<S2Loop>, List<S2Loop>> loopMap)
         {
             foreach (var key in loopMap.Keys)
             {
@@ -972,10 +971,10 @@ namespace Google.Common.Geometry
             }
         }
 
-        private static void InsertLoop(S2Loop newLoop, S2Loop parent, C5.IDictionary<S2Loop, List<S2Loop>> loopMap)
+        private static void InsertLoop(S2Loop newLoop, S2Loop parent, Dictionary<NullObject<S2Loop>, List<S2Loop>> loopMap)
         {
             List<S2Loop> children = null;
-            if (loopMap.Contains(parent))
+            if (loopMap.ContainsKey(parent))
                 children = loopMap[parent];
 
             if (children == null)
@@ -1000,7 +999,7 @@ namespace Google.Common.Geometry
             // Some of the children of the parent loop may now be children of
             // the new loop.
             List<S2Loop> newChildren = null;
-            if (loopMap.Contains(newLoop))
+            if (loopMap.ContainsKey(newLoop))
                 newChildren = loopMap[newLoop];
             for (var i = 0; i < children.Count;)
             {
@@ -1023,7 +1022,7 @@ namespace Google.Common.Geometry
             children.Add(newLoop);
         }
 
-        private void InitLoop(S2Loop loop, int depth, C5.IDictionary<S2Loop, List<S2Loop>> loopMap)
+        private void InitLoop(S2Loop loop, int depth, IDictionary<NullObject<S2Loop>, List<S2Loop>> loopMap)
         {
             if (loop != null)
             {
@@ -1031,7 +1030,7 @@ namespace Google.Common.Geometry
                 _loops.Add(loop);
             }
             List<S2Loop> children = null;
-            if (loopMap.Contains(loop))
+            if (loopMap.ContainsKey(loop))
                 children = loopMap[loop];
             if (children != null)
             {
