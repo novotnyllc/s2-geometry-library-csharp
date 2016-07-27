ebd9f1c11b94f104c9e95f52a2136be89a44bfb0
Ryan Currie <dddbliss@gmail.com>
2016-07-27 10:35:47 -0700

    Cleaning up unused using statements.

--- S2Geometry/S2Polygon.cs
+++ S2Geometry/S2Polygon.cs
@@ -1,10 +1,8 @@
-﻿using Google.Common.Geometry.DataStructures;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Google.Common.Geometry
 {
