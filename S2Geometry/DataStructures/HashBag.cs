880deeff88f8b8b0fe67ed888efeffbbdc460a7b
Oren Novotny <oren@novotny.org>
2016-07-27 21:35:09 -0400

    remove unused type

--- S2Geometry/DataStructures/HashBag.cs
+++ S2Geometry/DataStructures/HashBag.cs
@@ -93,46 +93,4 @@ namespace Google.Common.Geometry.DataStructures
         public int Count => size;
         public bool IsReadOnly => false;
     }
-
-    sealed class KeyValuePairEqualityComparer<K, V> : IEqualityComparer<KeyValuePair<K, V>>
-    {
-        readonly IEqualityComparer<K> keyequalityComparer;
-
-
-        /// <summary>
-        /// Create an entry equalityComparer using the default equalityComparer for keys
-        /// </summary>
-        public KeyValuePairEqualityComparer() { keyequalityComparer = EqualityComparer<K>.Default; }
-
-
-        /// <summary>
-        /// Create an entry equalityComparer from a specified item equalityComparer for the keys
-        /// </summary>
-        /// <param name="keyequalityComparer">The key equalityComparer</param>
-        public KeyValuePairEqualityComparer(IEqualityComparer<K> keyequalityComparer)
-        {
-            if (keyequalityComparer == null)
-                throw new NullReferenceException("Key equality comparer cannot be null");
-            this.keyequalityComparer = keyequalityComparer;
-        }
-
-
-        /// <summary>
-        /// Get the hash code of the entry
-        /// </summary>
-        /// <param name="entry">The entry</param>
-        /// <returns>The hash code of the key</returns>
-     
-        public int GetHashCode(KeyValuePair<K, V> entry) { return keyequalityComparer.GetHashCode(entry.Key); }
-
-
-        /// <summary>
-        /// Test two entries for equality
-        /// </summary>
-        /// <param name="entry1">First entry</param>
-        /// <param name="entry2">Second entry</param>
-        /// <returns>True if keys are equal</returns>
-        public bool Equals(KeyValuePair<K, V> entry1, KeyValuePair<K, V> entry2)
-        { return keyequalityComparer.Equals(entry1.Key, entry2.Key); }
-    }
 }
